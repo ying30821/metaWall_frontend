da5f174eeaafdc7c7b41efef94151be339b90840
losingone <ying30821@gmail.com>
2024-05-08 23:30:20 +0800

    feat: `Navbar`

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="bg-white">
+    <div class="container mx-auto px-4 py-3 flex justify-between items-center">
+      <h1 class="font-paytone-one text-2xl">MetaWall</h1>
+      <div class="flex gap-x-2.5">
+        <div class="rounded-full w-[30px] h-[30px] border border-black"></div>
+        <p class="pb-1 border-b-2 border-black font-bold">Member</p>
+      </div>
+    </div>
+  </nav>
+</template>
