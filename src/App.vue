e9ffa9cb5de769af660a900c45faf7271249cad0
losingone <ying30821@gmail.com>
2024-05-09 02:11:17 +0800

    feat: layout

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,19 @@
     <div v-if="['Login', 'Register'].includes($route.name)">
       <router-view />
     </div>
+    <div v-else class="relative">
+      <Navbar class="mb-12" />
+      <div class="container w-full mx-auto flex relative gap-x-7">
+        <div class="flex-1">
+          <router-view />
+        </div>
+        <Sidebar />
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Sidebar from '@/components/Sidebar.vue';
+</script>
