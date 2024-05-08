478e4404a4a58cea44a197ad909f754472b79765
losingone <ying30821@gmail.com>
2024-05-09 02:01:10 +0800

    feat: sticky `Navbar`

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,26 @@
 <template>
-  <nav class="bg-white">
+  <nav class="bg-white border-b-[3px] border-dark sticky top-0 left-0 z-10">
     <div class="container mx-auto px-4 py-3 flex justify-between items-center">
       <h1 class="font-paytone-one text-2xl">MetaWall</h1>
       <div class="flex gap-x-2.5">
-        <div class="rounded-full w-[30px] h-[30px] border border-black"></div>
-        <p class="pb-1 border-b-2 border-black font-bold">Member</p>
+        <div
+          class="rounded-full w-[30px] h-[30px] border-2 border-dark overflow-hidden"
+        >
+          <img
+            :src="userData.img"
+            alt="user_avatar"
+            class="object-cover aspect-square"
+          />
+        </div>
+        <p class="pb-1 border-b-2 border-dark font-bold">Member</p>
       </div>
     </div>
   </nav>
 </template>
+
+<script setup>
+const userData = {
+  name: '邊緣小杰',
+  img: 'https://fakeimg.pl/200x100/?retina=1&text=こんにちは&font=noto',
+};
+</script>
