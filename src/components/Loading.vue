9fd3022277b846c32ad71d86e12e6719f9e598ac
losingone <ying30821@gmail.com>
2024-05-14 00:36:56 +0800

    feat: `Loading` component

---
+++ src/components/Loading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    v-if="isLoading"
+    class="flex space-x-4 justify-center items-center bg-white/60 h-screen w-screen z-50 fixed top-0 left-0"
+  >
+    <span class="sr-only">Loading...</span>
+    <div
+      class="h-8 w-8 bg-primary rounded-full animate-bounce [animation-delay:-0.3s]"
+    />
+    <div
+      class="h-8 w-8 bg-primary rounded-full animate-bounce [animation-delay:-0.15s]"
+    />
+    <div class="h-8 w-8 bg-primary rounded-full animate-bounce" />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  isLoading: Boolean,
+});
+</script>
