acc011147e7f10ab09df4b65e0872ede2fde2a0b
losingone <ying30821@gmail.com>
2024-05-15 17:04:15 +0800

    feat: delete withCredentials

--- src/api/request.js
+++ src/api/request.js
@@ -4,7 +4,6 @@ const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 5000,
   headers: { 'Content-Type': 'application/json;charset=UTF-8' },
-  withCredentials: true,
 });
 
 instance.interceptors.request.use(
