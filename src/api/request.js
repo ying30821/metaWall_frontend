887be9692832bc4ed624646c08333f4737cd3267
losingone <ying30821@gmail.com>
2024-05-23 01:35:58 +0800

    feat: modify api timeout seconds

--- src/api/request.js
+++ src/api/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 5000,
+  timeout: 10000,
   headers: { 'Content-Type': 'application/json;charset=UTF-8' },
 });
 
