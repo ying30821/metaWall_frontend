c5fef71c228ed1fba2b8c869289823c330d50630
losingone <ying30821@gmail.com>
2024-05-22 23:17:23 +0800

    feat: modify `Login` & `Register` page css in different screen

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,35 +1,28 @@
 <template>
   <div class="w-screen h-screen flex items-center justify-center">
     <div
-      class="border-2 border-dark p-12 py-16 flex items-center gap-x-8 shadow-[-6px_6px_0_-0_#00040029] bg-light w-[52%]"
+      class="border-2 border-dark overflow-hidden px-6 py-8 lg:p-12 lg:py-16 max-w-[90%] flex items-center gap-x-8 shadow-[-6px_6px_0_-0_#00040029] bg-light xl:w-[52%] 2xl:w-[45%]"
     >
-      <div class="bg-login w-80 h-80 bg-center bg-contain bg-no-repeat" />
+      <div
+        class="bg-login w-60 h-60 lg:block lg:w-80 lg:h-80 bg-center bg-contain bg-no-repeat flex-shrink-0"
+      />
       <div class="flex-1">
-        <h1 class="text-6xl text-primary font-paytone-one mb-2 text-center">
+        <h1
+          class="text-4xl lg:text-6xl text-primary font-paytone-one mb-2 text-center"
+        >
           MetaWall
         </h1>
-        <p class="text-center text-2xl font-bold mb-9">
+        <p class="text-lg text-center lg:text-2xl font-bold mb-4 lg:mb-9">
           到元宇宙展開全新交友圈
         </p>
         <form action="#" class="mb-2 space-y-4">
           <div>
-            <input
-              type="email"
-              placeholder="Email"
-              class="block w-full border-0 py-3 text-gray-900 ring-2 ring-inset ring-black placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary px-5"
-            />
+            <input type="email" placeholder="Email" class="form-input" />
           </div>
           <div>
-            <input
-              type="password"
-              placeholder="Password"
-              class="block w-full border-0 py-3 text-gray-900 ring-2 ring-inset ring-black placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary px-5"
-            />
+            <input type="password" placeholder="Password" class="form-input" />
           </div>
-          <button
-            disabled
-            class="disabled:bg-gray-400 disabled:shadow-none disabled:border-gray-500/70 flex w-full p-4 justify-center rounded-lg bg-primary border-2 border-black px-4 py-3 text-white shadow-[-2px_2px_0_-0_rgba(0,0,0)] hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
-          >
+          <button class="btn-primary w-full shadow-[-2px_2px_0_-0_rgba(0,0,0)]">
             登入
           </button>
         </form>
